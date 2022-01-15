dde77a0c44e02badb9003cafd02c00bb2606479f
Alex Davies <alxdavies@googlemail.com>
2022-01-15 16:16:06 +0000

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # index - displays all tasks
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
 
-  # new - creates new task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  # create
-  post 'tasks', to: 'tasks#create'
+  # # new - creates new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # # create
+  # post 'tasks', to: 'tasks#create'
 
-  # show - displays a single task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # show - displays a single task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # edit - edits a single task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  # update
-  patch 'tasks/:id', to: 'tasks#update'
+  # # edit - edits a single task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # # update
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # destroy
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # destroy
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
